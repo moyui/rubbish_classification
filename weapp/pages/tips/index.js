9e6c90b7fedb5cb2b4676b2426b600491b694053
moyui <gxy1510@gmail.com>
2019-08-27 16:43:17 +0800

    fix change

--- weapp/pages/tips/index.js
+++ weapp/pages/tips/index.js
@@ -3,6 +3,19 @@ Page({
         active: 1,
         active1: 1
     },
+    onChange(event) {
+        switch (event.detail) {
+            case 0: wx.navigateTo({
+                url: '/pages/search/index'
+            }); break;
+            case 1: wx.navigateTo({
+                url: '/pages/tips/index'
+            }); break;
+            case 4: wx.navigateTo({
+                url: '/pages/tag/index'
+            }); break;
+        }
+    },
     onClick(event) {
         wx.showToast({
           title: event.detail.index + 1,   
