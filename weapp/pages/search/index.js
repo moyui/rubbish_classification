01452a22a9f095758471c5a3edda4c17a018c868
moyui <gxy1510@gmail.com>
2019-08-27 15:32:26 +0800

    录音

--- weapp/pages/search/index.js
+++ weapp/pages/search/index.js
@@ -1,29 +1,11 @@
+import Toast from '../../miniprogram_npm/vant-weapp/toast/toast';
+
 const RecorderManager = wx.getRecorderManager();
-
-const buffer = new ArrayBuffer(16)
-const int32View = new Int32Array(buffer);
-
-RecorderManager.onFrameRecorded((res) => {
-    const { frameBuffer, isLastFrame } = res
-    // int32View.push(frameBuffer)
-    // if (isLastFrame) {
-    // console.log(ab2str(frameBuffer))
-    // uploadMp3Buffer(frameBuffer, isLastFrame);
-    // }
-})
 
 RecorderManager.onStop((res) => {
     const { tempFilePath } = res;
     // uploadMp3Path(tempFilePath);
-    // uploadMp3Buffer()
 })
-
-// function ab2str(buf) {
-//     const str1 = String.fromCharCode.apply(null, buf);
-//     // const str2 = String.fromCharCode.apply(null, buf.slice(2, 4));
-//     console.log(str1)
-// }
-
 
 const uploadMp3Path = (filePath) => {
     const uploadTask = wx.uploadFile({
@@ -47,57 +29,53 @@ const uploadMp3Path = (filePath) => {
     })
 
 
-    // wx.request({
-    //     url: 'http://vip.yangxudong.broker.anjuke.test/work',
-    //     method: 'POST',
-    //     header: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     data: {
-    //         url: 5
-    //     }
-    // })
 }
-
-const uploadMp3Buffer = (buffer, isEnd) => {
-    console.log(int32View)
-    wx.request({
-        url: 'http://vip.yangxudong.broker.anjuke.test/work',
-        method: 'POST',
-        // header: {
-        //     "Content-Type": "application/x-www-form-urlencoded"
-        // },
-        header: {
-            "Content-Type": "application/json"
-        },
-        data: {
-            buffer: buffer,
-            isEnd: isEnd
-        }
-    })
-}
-
-
 
 Page({
     data: {
-        text: "This is page data.",
         active: 0,
-        buffer: new ArrayBuffer()
-    },
-    onChange(event) {
-        console.log(event.detail);
+        search: '',
+        loading: false,
+        isRecording: false
     },
     onRecordingStart() {
+        this.setData({
+            isRecording: true
+        })
         RecorderManager.start({
             sampleRate: 16000,
             numberOfChannels: 1,
             encodeBitRate: 32000,
-            format: 'mp3',
-            frameSize: 50
+            format: 'mp3'
         });
     },
     onRecordingEnd() {
+        this.setData({
+            isRecording: false
+        })
         RecorderManager.stop()
+    },
+    onChange(event) {
+        this.setData({
+            search: event.detail
+        })
+    },
+    onSearch() {
+        Toast.loading({
+            mask: true,
+            message: '加载中...',
+            duration: 0
+        });
+        wx.request({
+            url: 'http://vip.yangxudong.broker.anjuke.test/size',
+            method: 'GET',
+            data: {
+                keyword: this.data.search
+            },
+            success: (res) => {
+
+            },
+            complete: () => Toast.clear()
+        })
     }
 })
