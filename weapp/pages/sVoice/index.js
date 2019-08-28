34b4d5f2f4885ee3775eceeeebfc44ae72e73106
moyui <gxy1510@gmail.com>
2019-08-28 18:02:31 +0800

    add title

--- weapp/pages/sVoice/index.js
+++ weapp/pages/sVoice/index.js
@@ -1,46 +1,24 @@
 import Toast from '../../miniprogram_npm/vant-weapp/toast/toast';
 
-const RecorderManager = wx.getRecorderManager();
-
-RecorderManager.onStop((res) => {
-    const { tempFilePath } = res;
-    uploadMp3Path(tempFilePath);
-})
-
-const uploadMp3Path = (filePath) => {
-    Toast({
-        mask: false,
-        message: '上传中，请稍后'
-    });
-    const uploadTask = wx.uploadFile({
-        url: 'http://vip.yangxudong.broker.anjuke.test/work',
-        filePath,
-        header: {
-            "Content-Type": "multipart/form-data"
-        },
-        name: 'video',
-        success: function (res) {
-            console.log(res);
-            // Toast.success('上传成功');
-        },
-        fail: function (res) {
-            console.log(res);
-            // Toast.fail('上传失败');
-        }
-    })
-}
-
 Page({
+    Recorder: null,
     data: {
         loading: false,
         isRecording: false,
         userSearch: null
     },
+    onLoad() {
+        this.Recorder = wx.getRecorderManager();
+        this.Recorder.onStop((res) => {
+            const { tempFilePath } = res;
+            this.uploadMp3Path(tempFilePath);
+        })
+    },
     onRecordingStart() {
         this.setData({
             isRecording: true
         })
-        RecorderManager.start({
+        this.Recorder.start({
             format: 'mp3'
         });
     },
@@ -48,6 +26,44 @@ Page({
         this.setData({
             isRecording: false
         })
-        RecorderManager.stop()
-    }
+        this.Recorder.stop()
+    },
+    uploadMp3Path(filePath) {
+        Toast.loading({
+            mask: false,
+            message: '上传中...',
+            duration: 0
+        });
+        const uploadTask = wx.uploadFile({
+            url: 'http://vip.yangxudong.broker.anjuke.test/work',
+            filePath,
+            header: {
+                "Content-Type": "multipart/form-data"
+            },
+            name: 'video',
+            success: (res) => {
+                this.setData({
+                    userSearch: this.formatData(res.data)
+                })
+            },
+            fail: function (res) {
+            },
+            complete: () => Toast.clear()
+        })
+    },
+    formatData(data) {
+        const format = Object.keys(data).reduce((pre, item) => {
+            return [...pre, { name: item, type: data[item], css: this.getType(data[item]) }]
+        }, [])
+        return format
+    },
+    getType(type) {
+        switch (type) {
+            case 0: return 'card-unknown';
+            case 1: return 'card-retrive';
+            case 2: return 'card-bad';
+            case 3: return 'card-wet';
+            case 4: return 'card-dry';
+        }
+    },
 })
